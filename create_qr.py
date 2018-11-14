44a05ce834882ca72058f7127b2ece5c4f92616b
Daniel Ziorli <31424488+DonionZooimama@users.noreply.github.com>
2018-11-14 11:12:31 -0500

    Update create_qr.py

--- create_qr.py
+++ create_qr.py
@@ -1,27 +1,34 @@
 import pyqrcode
 import sys
-import qrtools
 import random
 
 database_directory = "database.csv"
 
-with open(database_directory, 'r') as file:
-    r = random.randint(100000000000, 999999999999)
-    run = True
-    while run:
-        run = False
-        for line in file:
-            if int(line.split(',')[0]) == r:
-                r = random.randint(100000000000, 999999999999)
-                run = True
-                break
+if len(sys.argv) == 1:
 
-with open(database_directory, 'a') as file:
-    line = "%s,,,,,,\n" % (r)
-    file.write(line)
-    print str(r) + " successfully created"
+    with open(database_directory, 'r') as file:
+        r = random.randint(100000000000, 999999999999)
+        run = True
+        while run:
+            run = False
+            for line in file:
+                if int(line.split(',')[0]) == r:
+                    r = random.randint(100000000000, 999999999999)
+                    run = True
+                    break
 
-qr = pyqrcode.create(str(r))
-qr.png("qrcodes/" + str(r) + ".png", scale=6)
+    with open(database_directory, 'a') as file:
+        line = "%s,,,,,,\n" % (r)
+        file.write(line)
+        print str(r) + " successfully created"
 
-print str(r) + ".png successfully created"
+    qr = pyqrcode.create(str(r))
+    qr.png("qrcodes/" + str(r) + ".png", scale=6)
+
+    print str(r) + ".png successfully created"
+
+else:
+    qr = pyqrcode.create(sys.argv[1])
+    qr.png("qrcodes/" + sys.argv[1] + ".png", scale=6)
+
+    print sys.argv[1] + ".png successfully created"
